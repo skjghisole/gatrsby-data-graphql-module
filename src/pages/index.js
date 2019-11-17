0c15160bab1cc291fd24a3a5276fbbb55a1de8dd
Stephen Karl Jeoffrey  Hisole <skjghisole@yahoo.com>
2019-11-16 21:01:05 -0500

    implemented the new gatsby-transform-remark to our gql query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
 import React from "react"
+import { graphql } from 'gatsby';
+import { css } from '@emotion/core';
+import { rhythm } from '../utils/typography'
+
 import Layout from "../components/Layout"
 
-export default () => (
+export default ({ data }) => (
 	<Layout>
 		<h1>Koalas can Sleep all day</h1>
 		<div>
@@ -12,5 +16,46 @@ export default () => (
 				alt="Random Koala on a tree"
 			/>
 		</div>
+		<div>
+			<h1
+				css={css`
+					display: inline-block;
+					border-bottom: 1px solid;
+				`}
+			>Amazing how Koalas look the same</h1>
+			<h4>
+				{data.allMarkdownRemark.totalCount} Posts	
+			</h4>
+			{data.allMarkdownRemark.edges.map(({ node }) => <div key={node.id}>
+				<h3 css={css`
+					margin-bottom: ${rhythm(1/4)}
+				`}>
+					{node.frontmatter.title}{" "}
+					<span  css={css`color:#bbb`}>
+						- {node.frontmatter.date}
+					</span>
+				</h3>	
+				<p>{node.excerpt}</p>
+			</div>)}
+		</div>
 	</Layout>
 )
+
+
+export const query = graphql`
+	query {
+		allMarkdownRemark (sort: { fields: [frontmatter___date], order: DESC}){
+			totalCount
+			edges {
+				node {
+					id
+					frontmatter {
+						title
+						date(formatString: "DD MMMM YYYY")
+					}
+					excerpt
+				}
+			}
+		}
+	}
+`
